c4dd27d49a4f249aab5431721646051485169195
navneet-rathi <nrathi@redhat.com>
2026-03-06 13:52:39 +0530

    updated added seq to confirm random

--- RandomStringExecutor.java
+++ RandomStringExecutor.java
@@ -4,16 +4,16 @@ import java.util.Random;
 public class RandomStringExecutor {
  public static void main(String[] args) {
         String[] thoughts = {
-            "Believe you can and you're halfway there.",
-            "Act as if what you do makes a difference.",
-            "Success is not final, failure is not fatal.",
-            "The only way to do great work is to love what you do.",
-            "Never limit yourself because of others' limited imagination.",
-            "The best time to plant a tree was 20 years ago.",
-            "Keep looking up – that's the secret of life.",
-            "The only limit to our realization of tomorrow is our doubts.",
-            "Do what you can, with what you have, where you are.",
-            "It always seems impossible until it's done."
+            "1. Believe you can and you're halfway there.",
+            "2. Act as if what you do makes a difference.",
+            "3. Success is not final, failure is not fatal.",
+            "4. The only way to do great work is to love what you do.",
+            "5. Never limit yourself because of others' limited imagination.",
+            "6. The best time to plant a tree was 20 years ago.",
+            "7. Keep looking up – that's the secret of life.",
+            "8. The only limit to our realization of tomorrow is our doubts.",
+            "9. Do what you can, with what you have, where you are.",
+            "10. It always seems impossible until it's done."
         };
 
         Random random = new Random();
